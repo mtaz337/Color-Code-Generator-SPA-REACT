745b1bc475d7b7500f1cda4b089925fd1c675a7d
mtaz337 <mahirtaz@gmail.com>
2023-10-23 13:05:12 +0600

    2nd commit

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,8 +1,40 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+
+const SingleColor = ({rgb,weight,index}) => {
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(',');
+  const hex = rgbToHex(...rgb)
+   
+  const handleClick = () =>{
+    setAlert(true);
+    navigator.clipboard.writeText(hex)
+    toast.success('Copied to Clipboard !', {
+      position: toast.POSITION.TOP_CENTER
+  });
+  }
+useEffect(()=>{
+const timeout = setTimeout(()=>{
+  setAlert(false);
+},1500)
+return ()=> clearTimeout(timeout)
+},[alert])
+
+  return (
+    <article className={`color ${index>10 && 'color-light'}`} style={{backgroundColor: `rgb(${bcg})`}} onClick={handleClick}>
+     <p className='perecent-value'>
+      {weight}%
+     </p>
+     <p className='color-value'>
+      {hex}
+     </p>
+     {alert && <ToastContainer
+/>}
+    </article>
+  )
 }
 
 export default SingleColor
